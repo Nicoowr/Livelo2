6713c568176b8fc6f09ea0715d85bc31cc3aa551
Nico <nicolas.li@hotmail.fr>
2017-10-27 19:49:17 +0200

    NewSensor implementation

---
+++ app/src/main/java/ch/livelo/livelo2/NewSensor.java
@@ -0,0 +1,18 @@
+package ch.livelo.livelo2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Nico on 27/10/2017.
+ */
+
+public class NewSensor extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_sensor);
+
+    }
+}
